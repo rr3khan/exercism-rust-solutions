f8b7b9292b21d1f6266e772f003434c3688a4b79
rr3khan <65298773+rr3khan@users.noreply.github.com>
2023-09-08 23:58:09 -0400

    Gigasecond solution iteration 2

--- exercises/practice/gigasecond/src/lib.rs
+++ exercises/practice/gigasecond/src/lib.rs
@@ -1,6 +1,14 @@
 use time::PrimitiveDateTime as DateTime;
+use time::OffsetDateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    let gigasecond = 1e9 as i64;
+    let dateInSeconds = start.assume_utc().unix_timestamp() as i64;
+    let totalSeconds = gigasecond + dateInSeconds;
+
+    let offset_date_time = OffsetDateTime::from_unix_timestamp(totalSeconds).unwrap();
+    let endDateTime = DateTime::new(offset_date_time.date(), offset_date_time.time());
+
+    return endDateTime;
 }
