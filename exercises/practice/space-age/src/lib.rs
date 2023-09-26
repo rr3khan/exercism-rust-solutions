69122b5b7d341184051954ce844c40379096e036
rr3khan <65298773+rr3khan@users.noreply.github.com>
2023-09-25 23:57:36 -0400

    exercise(space-age): Impl From for Duration

--- exercises/practice/space-age/src/lib.rs
+++ exercises/practice/space-age/src/lib.rs
@@ -2,19 +2,21 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    age_in_seconds: u64
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {s}")
+        Duration {
+            age_in_seconds: s, 
+        }
     }
 }
 
 pub trait Planet {
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({d:?}) to the number of years on this planet for that duration"
-        );
+        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
     }
 }
 
