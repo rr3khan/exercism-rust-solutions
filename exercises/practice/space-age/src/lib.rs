1c61713eca4e9b12297838fe377dfeebfd874ce3
rr3khan <65298773+rr3khan@users.noreply.github.com>
2023-11-23 23:36:26 -0500

    exercise:(space-age): Solution no Macro

--- exercises/practice/space-age/src/lib.rs
+++ exercises/practice/space-age/src/lib.rs
@@ -15,8 +15,9 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
+    const ORBITAL_PERIOD: f64;
     fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+        d.age_in_seconds as f64 / Self::ORBITAL_PERIOD
     }
 }
 
@@ -29,11 +30,13 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+const EARTH_SECONDS: f64 = 31557600.0;
+
+impl Planet for Mercury {const ORBITAL_PERIOD: f64 = 0.2408467*EARTH_SECONDS;}
+impl Planet for Venus {const ORBITAL_PERIOD: f64 =  0.61519726*EARTH_SECONDS;}
+impl Planet for Earth {const ORBITAL_PERIOD: f64 = 1.0*EARTH_SECONDS;}
+impl Planet for Mars {const ORBITAL_PERIOD: f64 = 1.8808158*EARTH_SECONDS;}
+impl Planet for Jupiter {const ORBITAL_PERIOD: f64 = 11.862615*EARTH_SECONDS;}
+impl Planet for Saturn {const ORBITAL_PERIOD: f64 =  29.447498 *EARTH_SECONDS;}
+impl Planet for Uranus {const ORBITAL_PERIOD: f64 =  84.016846*EARTH_SECONDS;}
+impl Planet for Neptune {const ORBITAL_PERIOD: f64 = 164.79132*EARTH_SECONDS;}
