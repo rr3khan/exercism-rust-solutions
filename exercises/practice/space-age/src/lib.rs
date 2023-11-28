e96bad8256ef0b12ee247e2d3e03caf8fbba9f85
rr3khan <65298773+rr3khan@users.noreply.github.com>
2023-11-27 22:12:29 -0500

    exercise(space-age): Solution with Macro

--- exercises/practice/space-age/src/lib.rs
+++ exercises/practice/space-age/src/lib.rs
@@ -32,11 +32,12 @@ pub struct Neptune;
 
 const EARTH_SECONDS: f64 = 31557600.0;
 
-impl Planet for Mercury {const ORBITAL_PERIOD: f64 = 0.2408467*EARTH_SECONDS;}
-impl Planet for Venus {const ORBITAL_PERIOD: f64 =  0.61519726*EARTH_SECONDS;}
-impl Planet for Earth {const ORBITAL_PERIOD: f64 = 1.0*EARTH_SECONDS;}
-impl Planet for Mars {const ORBITAL_PERIOD: f64 = 1.8808158*EARTH_SECONDS;}
-impl Planet for Jupiter {const ORBITAL_PERIOD: f64 = 11.862615*EARTH_SECONDS;}
-impl Planet for Saturn {const ORBITAL_PERIOD: f64 =  29.447498 *EARTH_SECONDS;}
-impl Planet for Uranus {const ORBITAL_PERIOD: f64 =  84.016846*EARTH_SECONDS;}
-impl Planet for Neptune {const ORBITAL_PERIOD: f64 = 164.79132*EARTH_SECONDS;}
+macro_rules! impl_trait_for {
+    ($Planet:ident $($t:ident($v:expr)),+)=> {
+        $(impl $Planet for $t{
+            const ORBITAL_PERIOD: f64 = $v * EARTH_SECONDS;
+        })+
+        
+    }
+}
+impl_trait_for!(Planet Mercury(0.2408467), Venus(0.61519726), Earth(1.0), Mars(1.8808158), Jupiter(11.862615), Saturn(29.447498), Uranus(84.016846), Neptune(164.79132));
